fe4c466eac616e6edca7e1e41ea8babcf238c9e9
Eggngineer <yama_shin1999@icloud.com>
2022-11-18 21:04:35 +0900

    init: return and handle an error is done.

--- hello/hello.go
+++ hello/hello.go
@@ -20,16 +20,45 @@
 // }
 
 // call-module-code :1
+// package main
+
+// import (
+//     "fmt"
+
+//     "example.com/greetings"
+// )
+
+// func main() {
+//     // Get a greeting message and print it.
+//     message := greetings.Hello("Eggngineer")
+//     fmt.Println(message)
+// }
+
+// return and handle an error
 package main
 
 import (
     "fmt"
-
+    "log"
     "example.com/greetings"
 )
 
 func main() {
-    // Get a greeting message and print it.
-    message := greetings.Hello("Eggngineer")
+    // Set properties of the predefined Logger, including
+    // the log entry prefix and a flag to disable printing
+    // the time, source file, and line number.
+    log.SetPrefix("greetings: ")
+    log.SetFlags(0)
+
+    // Request a greeting message.
+    message, err := greetings.Hello("")
+    // If an error was returned, print it to the console and
+    // exit the program.
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    // If no error was returned, print the returned message
+    // to the console.
     fmt.Println(message)
 }
