17d87045031eda8a9b50ee720df4f5361d4ccd3d
Eggngineer <yama_shin1999@icloud.com>
2022-11-18 20:57:11 +0900

    mod: done call-module-codes

--- hello/hello.go
+++ hello/hello.go
@@ -1,3 +1,4 @@
+// getting-started :1
 // package main
 
 // import "fmt"
@@ -6,12 +7,29 @@
 //     fmt.Println("Hello, World!")
 // }
 
+// getting-started :2
+
+// package main
+
+// import "fmt"
+
+// import "rsc.io/quote"
+
+// func main() {
+//     fmt.Println(quote.Go())
+// }
+
+// call-module-code :1
 package main
 
-import "fmt"
+import (
+    "fmt"
 
-import "rsc.io/quote"
+    "example.com/greetings"
+)
 
 func main() {
-    fmt.Println(quote.Go())
+    // Get a greeting message and print it.
+    message := greetings.Hello("Eggngineer")
+    fmt.Println(message)
 }
