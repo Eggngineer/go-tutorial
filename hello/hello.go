818feb4774f42bfc1eeb3d35e77da957898c7abc
Eggngineer <yama_shin1999@icloud.com>
2022-11-18 21:45:18 +0900

    init: done return a random greeting

--- hello/hello.go
+++ hello/hello.go
@@ -35,11 +35,41 @@
 // }
 
 // return and handle an error
+// package main
+
+// import (
+//     "fmt"
+//     "log"
+//     "example.com/greetings"
+// )
+
+// func main() {
+//     // Set properties of the predefined Logger, including
+//     // the log entry prefix and a flag to disable printing
+//     // the time, source file, and line number.
+//     log.SetPrefix("greetings: ")
+//     log.SetFlags(0)
+
+//     // Request a greeting message.
+//     message, err := greetings.Hello("")
+//     // If an error was returned, print it to the console and
+//     // exit the program.
+//     if err != nil {
+//         log.Fatal(err)
+//     }
+
+//     // If no error was returned, print the returned message
+//     // to the console.
+//     fmt.Println(message)
+// }
+
+// return a random greeting
 package main
 
 import (
     "fmt"
     "log"
+
     "example.com/greetings"
 )
 
@@ -51,7 +81,7 @@ func main() {
     log.SetFlags(0)
 
     // Request a greeting message.
-    message, err := greetings.Hello("")
+    message, err := greetings.Hello("Gladys")
     // If an error was returned, print it to the console and
     // exit the program.
     if err != nil {
